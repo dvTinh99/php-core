0668ed4e36d5dd6082c697dc7a465d3c0a4d9735
dvtinh99 <tinh.doan@codecomplete.jp>
2024-10-22 15:45:09 +0700

    update

--- Views/user_register.php
+++ Views/user_register.php
@@ -11,7 +11,7 @@
     <main>
       <h1>Upload a file</h1>
       <p>
-        <label for="file">File to upload</label><input type="file" id="file" />
+        <label for="file">File to upload</label><input type="file" id="file" multiple/>
       </p>
       <p>
         <progress />
@@ -32,68 +32,81 @@ addEventListener("DOMContentLoaded", () => {
   const log = document.querySelector("output");
   const abortButton = document.getElementById("abort");
 
-  fileInput.addEventListener("change", () => {
-    const xhr = new XMLHttpRequest();
-    xhr.timeout = 2000; // 2 seconds
+  fileInput.addEventListener("change", (event) => {
+    
+    let listRequest = []
+    for (let index = 0; index < fileInput.files.length; index++) {
+      const element = fileInput.files[index];
+      const request = uploadExcel(element, progressBar, abortButton, index)
 
-    // Link abort button
-    abortButton.addEventListener(
-      "click",
-      () => {
-        xhr.abort();
-      },
-      { once: true },
-    );
-
-    // When the upload starts, we display the progress bar
-    xhr.upload.addEventListener("loadstart", (event) => {
-      progressBar.classList.add("visible");
-      progressBar.value = 0;
-      progressBar.max = event.total;
-      log.textContent = "Uploading (0%)…";
-      abortButton.disabled = false;
-    });
-
-    // Each time a progress event is received, we update the bar
-    xhr.upload.addEventListener("progress", (event) => {
-      progressBar.value = event.loaded;
-      log.textContent = `Uploading (${(
-        (event.loaded / event.total) *
-        100
-      ).toFixed(2)}%)…`;
-    });
-
-    // When the upload is finished, we hide the progress bar.
-    xhr.upload.addEventListener("loadend", (event) => {
-      progressBar.classList.remove("visible");
-      if (event.loaded !== 0) {
-        log.textContent = "Upload finished.";
-      }
-      abortButton.disabled = true;
-    });
-
-    // In case of an error, an abort, or a timeout, we hide the progress bar
-    // Note that these events can be listened to on the xhr object too
-    function errorAction(event) {
-      progressBar.classList.remove("visible");
-      log.textContent = `Upload failed: ${event.type}`;
+      listRequest.push(request);
     }
-    xhr.upload.addEventListener("error", errorAction);
-    xhr.upload.addEventListener("abort", errorAction);
-    xhr.upload.addEventListener("timeout", errorAction);
-
-    // Build the payload
-    const fileData = new FormData();
-    fileData.append("file", fileInput.files[0]);
-
-    // Theoretically, event listeners could be set after the open() call
-    // but browsers are buggy here
-    xhr.open("POST", "/api/import", true);
-
-    // Note that the event listener must be set before sending (as it is a preflighted request)
-    xhr.send(fileData);
+    Promise.all(listRequest).then((values) => {
+      console.log('done all');
+    });
   });
 });
+
+async function uploadExcel(element, progressBar, abortButton, index = 0) {
+  // Link abort button
+  abortButton.addEventListener(
+    "click",
+    () => {
+      xhr.abort();
+    },
+    { once: true },
+  );
+
+  const xhr = new XMLHttpRequest();
+  //xhr.timeout = 20000; // 2 seconds
+  // When the upload starts, we display the progress bar
+  xhr.upload.addEventListener("loadstart", (event) => {
+    progressBar.classList.add("visible");
+    progressBar.value = 0;
+    progressBar.max = event.total;
+    console.log("Uploading (0%)…" + index);
+    abortButton.disabled = false;
+  });
+
+  // Each time a progress event is received, we update the bar
+  xhr.upload.addEventListener("progress", (event) => {
+    progressBar.value = event.loaded;
+    console.log(`${index}Uploading (${(
+      (event.loaded / event.total) *
+      100
+    ).toFixed(2)}%)…`);
+  });
+
+  // When the upload is finished, we hide the progress bar.
+  xhr.upload.addEventListener("loadend", (event) => {
+    progressBar.classList.remove("visible");
+    if (event.loaded !== 0) {
+      console.log(index + "Upload finished.");
+    }
+    abortButton.disabled = true;
+  });
+
+  // In case of an error, an abort, or a timeout, we hide the progress bar
+  // Note that these events can be listened to on the xhr object too
+  function errorAction(event) {
+    progressBar.classList.remove("visible");
+    console.log(`${index}Upload failed: ${event.type}`);
+  }
+  xhr.upload.addEventListener("error", errorAction);
+  xhr.upload.addEventListener("abort", errorAction);
+  xhr.upload.addEventListener("timeout", errorAction);
+
+  // Build the payload
+  const fileData = new FormData();
+  fileData.append("file", element);
+
+  // Theoretically, event listeners could be set after the open() call
+  // but browsers are buggy here
+  xhr.open("POST", "/api/import", true);
+
+  // Note that the event listener must be set before sending (as it is a preflighted request)
+  xhr.send(fileData);
+}
 
 </script>
 <style type="text/css">
